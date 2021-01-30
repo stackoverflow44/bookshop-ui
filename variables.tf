d174a2d9a22f266872527b394f6c775085a07b3a
Augustine Wah <augustine44@gmail.com>
2021-01-31 00:58:05 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,13 +6,11 @@
 variable "project" {
   description = "The project ID to host the site in."
   type        = string
-  
 }
 
 variable "website_domain_name" {
   description = "The name of the website and the Cloud Storage bucket to create (e.g. static.foo.com)."
   type        = string
- 
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
